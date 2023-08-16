0db7347a9fa4572a1c154e439df26c82d2606d8a
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-08-16 10:14:01 +0300

    fix(version): dynamic versioning

---
+++ kvault/__version__.py
@@ -0,0 +1,13 @@
+"""
+Contains version information about package
+"""
+import os
+
+__version__ = os.environ.get(
+    "PACKAGE_VERSION",
+    os.environ.get("CI_COMMIT_TAG",
+                   os.environ.get("GITHUB_REF_NAME",
+                                  os.environ.get("VERSION", "0.1-dev")
+                                  )
+                   )
+)
